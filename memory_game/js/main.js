700727521fb1e3c00f1ec67d6c2f113a165c220b
Tiffany Huddleston <tiffhuddleston@gmail.com>
2019-10-08 16:40:43 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,28 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardTwo);
-
-if (cardsInPlay.length === 2){
- if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 	alert("You found a match!");
 } else { 
 	alert("Sorry, try again.");
 }
 }
+
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
 
 
