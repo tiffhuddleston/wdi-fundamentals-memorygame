9532cafe10dd85ab62a0719fcb279a13a696a7db
Tiffany Huddleston <tiffhuddleston@gmail.com>
2019-10-09 12:58:18 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,26 @@
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
@@ -10,12 +32,17 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 }
 }
+
+
 
 flipCard(0);
 flipCard(2);
